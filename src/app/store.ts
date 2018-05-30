34e13db9beefe56dbeeafca962412508a3fd86a9
v761845 <Yamit.Villamil@united.com>
2018-05-30 11:58:40 -0500

    Add remove reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,23 +1,24 @@
-import { REMOVE_ITEM, ADD_ITEM } from "./actions";
+import { REMOVE_ITEM, ADD_ITEM } from './actions';
+import { tassign } from 'tassign';
 
 export interface IAppState {
-    todoList: Array<any>
+  todoList: Array<any>;
 }
 
-export const INITIAL_STATE:IAppState = {
+export const INITIAL_STATE: IAppState = {
     todoList: []
-}
+};
 
-export function rootReducer(state:IAppState, action):IAppState {
-    switch(action.type) {
+export function rootReducer(state: IAppState, action): IAppState {
+    switch (action.type) {
         case ADD_ITEM:
-            return { 
+            return {
                 todoList: state.todoList.concat({ title: action.title,
                 id: state.todoList.length + 1
-            })}
+            })};
         case REMOVE_ITEM:
             const target = state.todoList.filter(item => item.id !== action.id);
-            return null;
+            return tassign(state, { todoList: target });
     }
 
     return state;
